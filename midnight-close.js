5c2d7899a7a75fcbd07e6b3808957b7233ff70b9
Carlos Urtiz <101680429+charlie-urt@users.noreply.github.com>
2024-08-01 11:40:03 -0600

    Create midnight-close.js
    Triggers a function in the endpoint that closes all tracks after midnight

---
+++ midnight-close.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const SECRETS = require('./secrets');
+
+const server = axios.create({
+    baseURL: 'http://localhost:7000',
+});
+
+server.defaults.headers.common['Authorization'] = `Bearer ${SECRETS.token}`;
+
+server.get('/controlrecorrido/midnightCloseAll/')
+  .then((resp) => console.log(resp))
+  .catch((error) => {
+    if (error.response) {
+      console.error(`midnightCloseAll response error:: ${error.response.data}`);
+    } else if (error.request) {
+      console.error(`midnightCloseAll request error:: ${error.request}`);
+    } else {
+      console.error(`Something happened in setting up the request that triggered a midnightCloseAll error:: ${error.message}`);
+    }
+    console.error(error.config)
+  });
